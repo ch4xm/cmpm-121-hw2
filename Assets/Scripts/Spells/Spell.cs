25f1c882c3ca5e575a1351eab99b5a9ab8c3e69f
frankmd2 <147461071+frankmd2@users.noreply.github.com>
2026-05-11 23:05:49 -0700

    Matching Spell class to the spells.json, adding attributes. Create a helper Projectile class for easy deserialize

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -1,12 +1,39 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
+public class Projectile
+{
+    public int which;
+    public string trajectory;
+    public Vector3 where;
+    public Vector3 direction;
+    public float speed;
+    public Action<Hittable, Vector3> onHit;
+    public float? lifetime;
+
+    public void create()
+    {
+        if (lifetime.HasValue) 
+            GameManager.Instance.projectileManager.CreateProjectile(which, trajectory, where, direction, speed, onHit, lifetime.Value);
+        else 
+            GameManager.Instance.projectileManager.CreateProjectile(which, trajectory, where, direction, speed, onHit);
+    }
+}
+
 public class Spell 
 {
     public float last_cast;
     public SpellCaster owner;
+    public string name;
+    public string description;
+    public int icon;
+    public Damage damage;
+    public int mana_cost;
+    public float cooldown;
+    public Projectile projectile;
     public Hittable.Team team;
 
     public Spell(SpellCaster owner)
@@ -16,27 +43,27 @@ public class Spell
 
     public string GetName()
     {
-        return "Bolt";
+        return name;
     }
 
     public int GetManaCost()
     {
-        return 10;
+        return mana_cost;
     }
 
     public int GetDamage()
     {
-        return 100;
+        return damage.amount;
     }
 
     public float GetCooldown()
     {
-        return 0.75f;
+        return cooldown;
     }
 
     public virtual int GetIcon()
     {
-        return 0;
+        return icon;
     }
 
     public bool IsReady()
@@ -47,7 +74,7 @@ public class Spell
     public virtual IEnumerator Cast(Vector3 where, Vector3 target, Hittable.Team team)
     {
         this.team = team;
-        GameManager.Instance.projectileManager.CreateProjectile(0, "straight", where, target - where, 15f, OnHit);
+        projectile.create();
         yield return new WaitForEndOfFrame();
     }
 
@@ -55,7 +82,7 @@ public class Spell
     {
         if (other.team != team)
         {
-            other.Damage(new Damage(GetDamage(), Damage.Type.ARCANE));
+            other.Damage(new Damage(GetDamage(), damage.type));
         }
 
     }
