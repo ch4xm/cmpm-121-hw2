f3efd721bd3a5e474d9e08db13e52a6fc0768fee
Jay Tripathi <jatripat@ucsc.edu>
2026-05-20 16:49:41 -0700

    add basic relic class and folder, start creating accept spell UI

---
+++ Assets/Scripts/Relics/Relic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Relics
+{
+	public class Relic: MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
